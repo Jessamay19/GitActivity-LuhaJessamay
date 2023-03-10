6551d8d7d50c335684efe47e6bdd01dde9135a83
Jessamay <jessamayluha@gmail.com>
2023-03-10 12:27:56 +0800

    My Educational Background added

--- git-python-Luha.py
+++ git-python-Luha.py
@@ -1,3 +1,7 @@
 print ("Jessa May Nite Luha")
 print("Duyay Boac Marinduque")
 print ("January 19 2003")
+
+print ("College of Information and Computing Sciences")
+print ("Bachelor of Science and Information Technology")
+print ("2-B")
